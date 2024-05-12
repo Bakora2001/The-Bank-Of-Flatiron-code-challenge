7179083f424150c929ffc0b1140dc78d18a4ed9a
Maxwel <maxwellbakora@gmail.com>
2024-05-12 11:23:01 +0300

    Bank of Flatiron code challenge complete

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { Box, Heading } from "@chakra-ui/react";
+import Foot from "./components/foot/Foot";
+
+
+function App() {
+  return(
+  <>
+  <Box className="body">
+    <Heading className="header">The Royal Bank of Flatiron</Heading>
+   <Foot/>
+    </Box> 
+    </>
+ )
+}
+
+export default App;
+
+
+
+
+
+
+
+ 
+
+
+
+
